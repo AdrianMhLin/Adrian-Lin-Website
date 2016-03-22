bc245201c238683f61c8beafb565e4328a60cf89
AdrianMLin <adrian.mh.lin@gmail.com>
2016-03-22 18:36:37 -0400

    fixed footer's weird positioning with clearfix

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -7,3 +7,10 @@ function stickFooterAtBottom(desktopVersionId, mobileVersionId) {
 							.css('position', 'fixed');
 
 }	
+
+function highlightNavPage(){
+	var thisPageName = $('main').data('pageName'),
+	    navItem = $('#primary-nav').find('a:contains("' + thisPageName + '")');
+	// navItem.attr('class', 'active'); /* doesnt work in heroku. why?? */
+	navItem.css('color', 'turquoise');
+}
