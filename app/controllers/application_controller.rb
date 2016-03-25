5c7aff446676861a6365bc563f810414895af2c7
AdrianMLin <adrian.mh.lin@gmail.com>
2016-03-25 00:35:09 -0400

    fixed naked url redirection issue using Refraction gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Refraction.configure do |req|
+  #   if req.host == "adrianlin.com"
+  #     req.permanent! :host => "www.adrianlin.com"
+  #   end
+  # end
+
 end
