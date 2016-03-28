2f8270ec7e874cd1cd4a11624a48df035a3be8de
AdrianMLin <adrian.mh.lin@gmail.com>
2016-03-27 23:05:32 -0400

    added ability to get pdf

--- app/controllers/work_controller.rb
+++ app/controllers/work_controller.rb
@@ -7,20 +7,36 @@ class WorkController < ApplicationController
     render :portfolio
   end
 
+
+
   def yelp
     render :yelp
   end
+  def yelp_presentation
+    render :yelp_presentation
+  end
+
 
   def inked_voices
     render :inked_voices
   end
+  def inked_voices_presentation
+    render :inked_voices_presentation
+  end
+
+
+
+  def linkedin
+    render :linkedin
+  end
+  def linkedin_presentation
+    render :linkedin_presentation
+  end
+
 
   def personal_website
     render :personal_website
   end
 
-  def linkedin
-    render :linkedin
-  end
 
 end
